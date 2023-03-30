32fc6468a00c8ffd1181024f486b574c001c1e4a
jonathan baril demanche <38406432+9338088@users.noreply.github.com>
2023-03-30 19:54:01 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
     <title>File Uploader</title>
 </head>
-<link type="text/css" rel="stylesheet" media="screen" href="css/.xd.css" />
+<link type="text/css" rel="stylesheet" media="screen" href="/css/.xd.css" />
 <body>
     <div class="noise"></div>
     <div class="overlay"></div>
@@ -10,20 +10,37 @@
         <h1>File <span class="errorcode">Uploader</span></h1>
 	Maximum file size 20MB
         <!-- uploader code start her -->
-		<form action="upload.php" method="post" enctype="multipart/form-data">
-		<input type="file" name="fileToUpload">
-		<input type="submit" name="submit" value="Upload">
-	</form>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <select name="folder">
+                <?php foreach (glob("uploads/*", GLOB_ONLYDIR) as $dir) {
+                    $folderName = basename($dir);
+                    echo "<option value=\"$folderName\">$folderName</option>";
+                } ?>
+            </select>
+            <input type="submit" value="Upload" name="submit">
+        </form>
         <!-- uploader code end her -->
-<!-- message venant de upload.php -->
+<!-- message venant des script php -->
         <div id="message">
             <?php if (isset($_GET["message"])) {
                 echo $_GET["message"];
             } ?>
         </div>
-<!--fin de message venant de upload.php -->
+<!--fin de message venant des script php -->
+
         <!-- send user to files --> 
         <p class="output">Go to<a href="uploads/">Uploads</a>.</p>
+
+        <!-- form to create folder -->
+        <h1>Create Folder.</h1>
+        <form action="createfolder.php" method="post">
+            <label for="foldername">Folder Name:</label>
+            <input type="text" name="foldername" id="foldername">
+            <input type="submit" name="createfolder" value="Create Folder">
+        </form>
+        <!-- end form to create folder --> 
+
     </div>
     <body>
 </html>
